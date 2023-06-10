fa4ae9f21f5d55e103ec4d2f85891e545798a807
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-06-09 23:31:29 -0500

    enlaze

---
+++ src/main/java/com/istateca/app/IstatecaAppApplication.java
@@ -0,0 +1,13 @@
+package com.istateca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstatecaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstatecaAppApplication.class, args);
+	}
+
+}
